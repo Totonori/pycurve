89a7ec661c72f3c13e4f2823c16278ebaf096853
Totonori <torokiki09@gmail.com>
2025-10-16 00:11:12 -0700

    v2 final commit (hopefully)

--- midterm_project_Brandon_Sanchez.py
+++ midterm_project_Brandon_Sanchez.py
@@ -43,25 +43,84 @@ def user_data():
         i = float(i_value)
         collected_data.append((v, i)) #pairs the x,y together, (v,i)
     print(f"The collected data is: {collected_data}")
+
+    #to allow the user to save their data points as a csv file
+    with open("user_data.csv", "w") as file: #does this make a csv file in the same directory as the python file?
+        #tested, it does!!
+        file.write("Voltage (V), Current (A)\n") #titles of the the two datasets
+        for v, i in collected_data:
+            file.write(f"{v}, {i}\n") #writes the data in the csv file
     return collected_data
-def solar_graph():
+#we're so back
+#def solar_graph():
+    generate_solar_graph = solar_cell.get()
+    if generate_solar_graph == False:
+        return
+    if generate_solar_graph == True:
+        voltages_solar = np.linspace(0, 5.0, 100)
+        currents_solar = 0.5 * (1 - np.exp(voltages_solar - 5.0)) #close to real solar cell curve
+#def resistor_graph():
     ...
-def resistor_graph():
-    ...
-def capacitor_graph():
+#def capacitor_graph():
     ...
 def user_graph():
+    #resistor = resistor_graph()
+    #capacitor = capacitor_graph()
     data = user_data() #calls back the user_data function to get the data
     voltages = [v for v, i in data] #creates a list of just the voltages
     currents = [i for v, i in data] #creates a list of just the currents
 
-    plt.plot(voltages, currents) #plots the two lists as x and y
-    plt.scatter(voltages, currents, color='red') #marks the actual points plotted
-    plt.title("I-V Curve")#title of the graph
-    plt.xlabel("Voltage (V)") #x axis label 
-    plt.ylabel("Current (A)") #y axis label
-    plt.show() #shows the graph
-
+## before adding checkboxes, this was the original graphing code
+    #plt.plot(voltages, currents, color='blue') #plots the two lists as x and y
+    #plt.scatter(voltages, currents, color='red') #marks the actual points plotted
+    #plt.title("I-V Curve")#title of the graph
+    #plt.xlabel("Voltage (V)") #x axis label 
+    #plt.ylabel("Current (A)") #y axis label
+    #plt.show() #shows the graph
+    if solar_cell.get() == False and resistor.get() == False and capacitor.get() == False: # case in which there is none selected, user wants to make their own graph
+        plt.plot(voltages, currents, color='blue', label='User Graph') #plots the two lists as x and y
+        plt.scatter(voltages, currents, color='red') #marks the actual points plotted
+        plt.title("I-V Curve")#title of the graph
+        plt.xlabel("Voltage (V)") #x axis label 
+        plt.ylabel("Current (A)") #y axis label
+        plt.legend() #to show what line is what
+        plt.show() #shows the graph
+    if solar_cell.get() == True:
+        voltages_solar = np.linspace(0, 5.0, 100)
+        currents_solar = 0.075 * (1 - np.exp(voltages_solar - 5.0)) #close to real solar cell curve, based on 75mA max current on observations made in EE001 class
+        plt.plot(voltages, currents, color='blue', label='User Graph') #plots the two lists as x and y
+        plt.plot(voltages_solar, currents_solar, color='orange', label='Solar Cell') #plots the solar cell graph
+        plt.scatter(voltages, currents, color='red') #marks the actual points plotted
+        plt.title("I-V Curve with Solar Cell Comparison")#title of the graph
+        plt.xlabel("Voltage (V)") #x axis label 
+        plt.ylabel("Current (A)") #y axis label
+        plt.legend() #to show what line is what
+        plt.show() #shows the graph
+    if resistor.get() == True:
+        voltages_resistor = np.linspace(0, 5.0, 100)
+        currents_resistor = voltages_resistor / 10.0 #Ohm's Law with R=10 ohms
+        plt.plot(voltages, currents, color='blue', label='User Graph') #plots the two lists as x and y
+        plt.plot(voltages_resistor, currents_resistor, color='green', label='Resistor (R=10Ω)') #plots the resistor graph
+        plt.scatter(voltages, currents, color='red') #marks the actual points plotted
+        plt.title("I-V Curve with Resistor Comparison")#title of the graph
+        plt.xlabel("Voltage (V)") #x axis label 
+        plt.ylabel("Current (A)") #y axis label
+        plt.legend() #to show what line is what
+        plt.show() #shows the graph
+    if capacitor.get() == True:
+        voltages_capacitor = np.linspace (0, 5.0, 100)
+        #more complex I-V curve for capacitor, not linear
+        #currents_capacitor = 0.02 * np.sin(voltages_capacitor * (np.pi / 5)) #simple sinusoidal approximation for capacitor I-V curve (THIS WAS GENERATED FOR CURIOSITY PURPOSES, IT WILL NOT APPEAR IN THE FINAL RESULTS BECAUSE IT'S NOT MY WORK)
+        currents_capacitor = 0.01 * voltages_capacitor #simple approximation for capacitor I-V curve
+        plt.plot(voltages, currents, color='blue', label='User Graph') #plots the two lists as x and y
+        plt.plot(voltages_capacitor, currents_capacitor, color='purple', label='Capacitor') #plots the capacitor graph
+        plt.scatter(voltages, currents, color='red') #marks the actual points plotted
+        plt.title("I-V Curve with Capacitor Comparison")#title of the graph
+        plt.xlabel("Voltage (V)") #x axis label
+        plt.ylabel("Current (A)") #y axis label
+        plt.legend() #to show what line is what
+        plt.show() #shows the graph
+    
 text_voltage = Label(root, text="Voltage (V)") #text that labels the voltage point side (x)
 text_voltage.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
 
@@ -111,6 +170,7 @@ check_solar = Checkbutton(root, text="Solar Cell", variable=solar_cell) #creates
 check_solar.grid(row=0, column=1, pady=60, padx=0, sticky="s") #placement of checkbox
 check_resistor = Checkbutton(root, text="Resistor", variable=resistor) #creates checkbox for resistor
 check_resistor.grid(row=0, column=1, pady=30, padx=0, sticky="s")
+#but what if they use a different value resistor? maybe have a text box pop up if checked? could be for a future iteration
 check_capacitor = Checkbutton(root, text="Capacitor", variable=capacitor) #creates checkbox for capacitor
 check_capacitor.grid(row=0, column=1, pady=0, padx=0, sticky="s")
 
